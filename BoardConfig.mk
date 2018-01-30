1ebab6ca29ab5d69e2cf8c45abde71c2f6b331b8
ripee <ripee.dev@gmail.com>
2018-01-30 10:56:32 -0500

    initial commit of branch android-7.1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,71 +1,69 @@
-#
-# Copyright (C) 2013 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+DEVICE_TREE := device/samsung/i9305
 
-# This variable is set first, so it can be overridden
-# by BoardConfigVendor.mk
+CM_PLATFORM_SDK_VERSION := 7	# Required for libf2fs.so
+override TARGET_OUT_VENDOR_SHARED_LIBRARIES = $(TARGET_OUT_SHARED_LIBRARIES)
 
--include device/samsung/smdk4412-common/BoardCommonConfig.mk
--include device/samsung/smdk4412-qcom-common/BoardCommonConfig.mk
+TARGET_BOOTANIMATION_PRELOAD := true
 
-LOCAL_PATH := device/samsung/i9305
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_VARIANT := cortex-a9
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+BOARD_GLOBAL_CFLAGS += -DEXYNOS4_ENHANCEMENTS
+BOARD_GLOBAL_CFLAGS += -DEXYNOS4X12_ENHANCEMENTS
 
-# RIL
-COMMON_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND='{ "ril.ks.status", AID_SYSTEM, 0 },'
+BOARD_VENDOR := samsung
+TARGET_BOARD_PLATFORM := exynos4
+TARGET_SOC := exynos4x12
+TARGET_BOOTLOADER_BOARD_NAME := smdk4x12
 
-# Camera
-COMMON_GLOBAL_CFLAGS += -DCAMERA_WITH_CITYID_PARAM
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
 
-TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
+TARGET_PROVIDES_INIT := true
+TARGET_PROVIDES_INIT_TARGET_RC := true
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
-TARGET_KERNEL_CONFIG := ../../../../../../device/samsung/i9305/kernelconfig/cyanogenmod_i9305_defconfig
-# TARGET_PREBUILT_KERNEL := device/samsung/i9305/prebuild/kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/kernel
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_TREE)/mkbootimg.mk
+BOARD_KERNEL_CMDLINE := console=ttySAC2,115200
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_PAGESIZE := 2048
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 
-# Recovery
-TARGET_RECOVERY_FSTAB := device/samsung/i9305/rootdir/twrp.fstab
-RECOVERY_FSTAB_VERSION := 2
+# Filesystem
+BOARD_NAND_PAGE_SIZE := 4096
+BOARD_NAND_SPARE_SIZE := 128
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12381585408
+BOARD_FLASH_BLOCK_SIZE := 4096
+TARGET_USERIMAGES_USE_EXT4 := true
 
-# TWRP
-TWRP_NEW_THEME := true
+LZMA_RAMDISK_TARGETS := boot,recovery
+
+# TWRP specific build flags
 TW_THEME := portrait_hdpi
-TW_INCLUDE_CRYPTO := true
-
-TARGET_USERIMAGES_USE_F2FS := true
-RECOVERY_SDCARD_ON_DATA := true
 BOARD_HAS_NO_REAL_SDCARD := true
-HAVE_SELINUX := true
+TW_NO_REBOOT_BOOTLOADER := true
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 162
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
-TW_MAX_BRIGHTNESS := 255
-TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+TW_NO_EXFAT_FUSE := true
+TW_MTP_DEVICE := "/dev/mtp_usb"
+TW_EXCLUDE_SUPERSU := true
 
-
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/i9300/custombootimg.mk
-
-
-# assert
-TARGET_OTA_ASSERT_DEVICE := m3,m3xx,i9305,GT-I9305
-
-# inherit from the proprietary version
--include vendor/samsung/i9305/BoardConfigVendor.mk
-
-# Selinux
-BOARD_SEPOLICY_DIRS += device/samsung/i9305/selinux
-BOARD_SEPOLICY_DIRS += device/samsung/i9305/cm12chroot/selinux
+# Encryption support
+TW_INCLUDE_CRYPTO := true
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# i9305-cm13-device
+## TWRP device tree for Samsung Galaxy S III (International LTE)
+## i9305
+
+Add to `.repo/local_manifests/i9305.xml`:
+
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+  <project name="TeamWin/android_device_samsung_i9305" path="device/samsung/i9305" remote="github" revision="android-7.1" />
+</manifest>
+```
+
+Then run `repo sync` to check it out.
+
+To build:
+
+```sh
+. build/envsetup.sh
+lunch omni_i9305-eng
+mka recoveryimage
+```
+
+Kernel source: https://github.com/LineageOS/android_kernel_samsung_smdk4412/tree/cm-14.1
